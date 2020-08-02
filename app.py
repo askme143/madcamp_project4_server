ac4d2ce71b0077fefa90b2f272cb001086f4e94e
Ubuntu <ubuntu@ip-172-31-42-223.ap-northeast-2.compute.internal>
2020-08-02 14:46:54 +0000

    Implement youtube api

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
+import os
 import youtube
+from pytube import YouTube
 from pymongo import MongoClient
-from flask import Flask, render_template, request, url_for, send_from_directory
+from flask import Flask, render_template, request, url_for, send_from_directory, make_response, send_file
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
 
 db = client.prj4
+wav_path = os.getcwd() + "/static/temp/youtube_wav"
 
 # Sample
 @app.route('/')
@@ -17,16 +20,80 @@ def sample_input():
     return render_template('sample_input.html')
 
 # Test
-@app.route('/test/youtube', methods=['POST'])
-def test_youtube():
-    print("> Test youtube")
-    url = request.form['youtube_url']
-    youtube.get_audio(url)
-    return "hello world"
-
 @app.route('/test')
 def test_():
     return "hello world"
+
+@app.route('/api/youtube/upload')
+def youtube_upload():
+    print("> Youtube Upload")
+    try:
+        url = request.args.get('url')
+        title = request.args.get('title')
+        email = request.args.get('email')
+        filename = youtube.get_audio(url)
+
+        if (title == None):
+            title = YouTube(url).title
+        if (email == None):
+            email = ""
+         
+        collection = db.audio
+        my_document = {'url': url, 'email': email, 'title': title, 'filename': filename}
+        try:
+            collection.insert_one(my_document)
+        except:
+            return "duplicated"
+        
+        return "success"
+    except:
+        return "fail", 500
+
+@app.route('/api/youtube/download')
+def youtube_download():
+    print("> Youtube Download")
+    global wav_path
+
+    title = request.args.get('title')
+    url = request.args.get('url')
+    email = request.args.get('email')
+
+    collection = db.audio
+    my_query = dict()
+
+    if (title != None):
+        my_query['email'] = email
+        my_query['title'] = title
+
+        result = collection.find_one(my_query)
+
+        if (result == None):
+            return "Invalid request", 500
+        else:
+            return send_file(wav_path + "/" + result['filename'] + ".wav")
+    elif (url != None):
+        my_query['url'] = url
+
+        result = collection.find_one(my_query)
+        if (result == None):
+            try:
+                url = request.args.get('url')
+                title = YouTube(url).title
+                email = ""
+                filename = youtube.get_audio(url)
+                
+                collection = db.audio
+                my_document = {'url': url, 'email': email, 'title': title, 'filename': filename}
+                try:
+                    collection.insert_one(my_document)
+                finally:
+                    return send_file(wav_path + "/" + filename + ".wav")
+            except:
+                return "Invalid request", 500
+        else:
+            return send_file(wav_path + "/" + result['filename'] + ".wav")
+    else:
+        return "Invalid request", 500
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=8000, debug=True)
--- youtube.py
+++ youtube.py
@@ -9,12 +9,17 @@ mp3_path = os.getcwd() + "/app/static/temp/youtube_mp3"
 wav_path = os.getcwd() + "/app/static/temp/youtube_wav"
 
 def get_audio(url: str):
-    get_wav_from_mp3(get_mp3(url))
+    return get_wav_from_mp3(get_mp3(url))
 
 def get_mp3(url: str):
     yt = YouTube(url)
     filename = str(int(time.time())) + yt.title
     filename = re.sub('[^0-9a-zA-Zㄱ-힗]', '', filename)
+
+    # Check whether it's already in the directory
+    filenames = os.listdir(mp3_path)
+    if filename in filenames:
+        return filename
 
     yt.streams.get_audio_only().download(mp3_path, filename)
     os.rename(mp3_path + "/" + filename + ".mp4", mp3_path + "/" + filename + ".mp3")
@@ -25,6 +30,8 @@ def get_wav_from_mp3(filename: str):
     AudioSegment.converter = which("ffmpeg")
     sound = AudioSegment.from_file(mp3_path + "/" + filename + ".mp3", format="mp4")
     sound.export(wav_path + "/" + filename + ".wav", format="wav")
+
+    return filename
 
 if __name__ == '__main__':
     get_audio('https://www.youtube.com/watch?v=MFkJ0EhumYU&list=RDghNci2gUpSk&index=8')
